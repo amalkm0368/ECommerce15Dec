0747aa1e62df51cc02019b026ec955538064b674
amal <amalkm0368@gmail.com>
2025-12-16 21:57:00 +0530

    Initial commit

---
+++ client/src/utils/axiosConfig.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const apiAxios = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api",
+})
+
+apiAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default apiAxios
+
+export const backend_url = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/"
+ 
